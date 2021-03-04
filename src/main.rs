fd406d9ef310eef28f90d356bf0cd52fa752aaa4
Michael Abel <info@abel-ikt.de>
2021-03-04 11:54:21 +0100

    remove things

--- src/main.rs
+++ src/main.rs
@@ -21,39 +21,6 @@ use trait_object_version::trait_object_mod;
 // cargo run && cargo test
 // cargo run && cargo test -- --show-output
 
-// Related:
-
-// https://stackoverflow.com/questions/55152927/how-to-mock-specific-methods-but-not-all-of-them-in-rust
-// "As you have already learned, you cannot replace methods on a type.
-// The only thing you can do is move the methods to a trait and then
-// provide production and test-specific implementations of that trait.
-// How you structure the trait determines the granularity of what you
-// are able to test.
-
-// https://knowitlabs.no/rust-2020-testing-4ab3d80112ba
-
-/*
-* Within my team it has been debated whether the best style is to
-* exclusively depend on abstract interfaces, everywhere. My personal
-* view is that this should be determined by the runtime intent. In high
-* level languages this is usually no problem, because method dispatch is
-* usually dynamic dy default. This means that any or most concrete types
-* or classes are easily sub-classable, and thus the coupling to such a
-* class by name is not really that strong under the hood. All
-* python/Java/Kotlin/JS languages support this easily: Creating mocks of
-* concrete types.
-* ...
-* So the way to express an abstraction in Rust is to use a trait.
-* Trait mocking in Rust today is fairly developer friendly, but not as
-* friendly as in dynamic-dispatch-languages. What I’ve been doing so
-* far is to introduce a new trait everywhere I need test isolation,
-* and use the crate mockall to autogenerate a mocked implementation
-* that I instantiate in my test. Not very far from how java/mockito or
-* jest works, except that, as mentioned, we always need to be very
-* explicit about the abstraction taking place:
-*/
-
-
 /*
 *    Use-Case: Test a fan controller that depends on a non deterministic sensor
 *
