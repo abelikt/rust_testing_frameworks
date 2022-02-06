3fe5fa63d98cfb22a6e74afe60a93a02bd57292c
Michael Abel <info@abel-ikt.de>
2022-02-06 13:44:43 +0100

    Copy vector definition from polish

--- polish_experiment/src/main.rs
+++ polish_experiment/src/main.rs
@@ -1,12 +1,12 @@
 /*
  * https://crates.io/crates/polish
- *
+ * https://github.com/Alkass/polish
  */
 
 extern crate polish;
 
 use polish::logger::Logger;
-use polish::test_case::{TestCase, TestCaseStatus, TestRunner};
+use polish::test_case::{TestCase, TestCaseStatus, TestRunner, Testable};
 
 fn my_test_case(logger: &mut Logger) -> TestCaseStatus {
     // TODO: Your test case code goes here
@@ -31,6 +31,31 @@ fn test_stuff(logger: &mut Logger) -> TestCaseStatus {
     logger.warn(format!("about to fail"));
     logger.fail(format!("failed with err_code: {code}", code = -1));
     TestCaseStatus::PASSED
+}
+
+struct MyTestCase;
+
+impl Testable for MyTestCase {
+    fn tests(self) -> Vec<TestCase> {
+        vec![
+            TestCase::new(
+                "Some Title #1",
+                "Testing Criteria",
+                Box::new(|logger: &mut Logger| -> TestCaseStatus {
+                    // TODO: Your test case goes here
+                    TestCaseStatus::PASSED
+                }),
+            ),
+            TestCase::new(
+                "Some Title #2",
+                "Testing Criteria",
+                Box::new(|logger: &mut Logger| -> TestCaseStatus {
+                    // TODO: Your test case goes here
+                    TestCaseStatus::SKIPPED
+                }),
+            ),
+        ]
+    }
 }
 
 fn main() {
@@ -63,4 +88,6 @@ fn main() {
     ));
 
     another_runner.run_test(TestCase::new("", "", Box::new(test_stuff)));
+
+    TestRunner::new().run_tests_from_class(MyTestCase {});
 }
