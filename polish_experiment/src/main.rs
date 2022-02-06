a5e9933958a8e05b7eec030ba3d3879b4bdd8c8c
Michael Abel <info@abel-ikt.de>
2022-02-06 13:03:26 +0100

    Add another test and format

--- polish_experiment/src/main.rs
+++ polish_experiment/src/main.rs
@@ -1,4 +1,3 @@
-
 /*
  * https://crates.io/crates/polish
  *
@@ -6,15 +5,29 @@
 
 extern crate polish;
 
-use polish::test_case::{TestRunner, TestCaseStatus, TestCase};
 use polish::logger::Logger;
+use polish::test_case::{TestCase, TestCaseStatus, TestRunner};
 
 fn my_test_case(logger: &mut Logger) -> TestCaseStatus {
-  // TODO: Your test case code goes here
-  TestCaseStatus::PASSED // Other valid statuses are (FAILED, SKIPPED, and UNKNOWN)
+    // TODO: Your test case code goes here
+    TestCaseStatus::PASSED // Other valid statuses are (FAILED, SKIPPED, and UNKNOWN)
+}
+
+fn another_test_case(logger: &mut Logger) -> TestCaseStatus {
+    TestCaseStatus::PASSED // Other valid statuses are (FAILED, SKIPPED, and UNKNOWN)
 }
 
 fn main() {
-  let test_case = TestCase::new("Test Case Title", "Test Case Criteria", Box::new(my_test_case));
-  TestRunner::new().run_test(test_case);
+    let test_case = TestCase::new(
+        "Test Case Title",
+        "Test Case Criteria",
+        Box::new(my_test_case),
+    );
+    let another_test_case = TestCase::new(
+        "Test Case Title",
+        "Test Case Criteria",
+        Box::new(another_test_case),
+    );
+    TestRunner::new().run_test(test_case);
+    TestRunner::new().run_test(another_test_case);
 }
