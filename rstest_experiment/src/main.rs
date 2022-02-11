4ff146410ee6b0be873447d3aec0ae3628dba065
Michael Abel <info@abel-ikt.de>
2022-02-11 16:08:07 +0100

    Copy example with fixture inheritance and make it run

--- rstest_experiment/src/main.rs
+++ rstest_experiment/src/main.rs
@@ -51,3 +51,54 @@ fn second_single(once_fixture: &i32) {
     // function result.
     assert_eq!(&42, once_fixture)
 }
+
+
+
+// Fixtures that call other fixtures seem to be unused for LSP
+
+
+struct User {name: String, age: u8}
+
+impl User {
+
+    fn new(name:&str, age:u8) -> User {
+        User{name:name.to_string(), age:age}
+    }
+
+    fn name(&self) -> &str {
+        &self.name
+    }
+
+    fn age(&self) -> u8 {
+        self.age
+    }
+}
+
+
+#[fixture]
+fn user(#[default("Alice")] name: &str, #[default(22)] age: u8) -> User {
+    User::new(name, age)
+}
+
+#[rstest]
+fn is_alice(user: User) {
+    assert_eq!(user.name(), "Alice")
+}
+
+#[rstest]
+fn is_22(user: User) {
+    assert_eq!(user.age(), 22)
+}
+
+#[rstest]
+fn is_bob(#[with("Bob")] user: User) {
+    assert_eq!(user.name(), "Bob")
+}
+
+#[rstest]
+fn is_42(#[with("", 42)] user: User) {
+    assert_eq!(user.age(), 42)
+}
+
+
+
