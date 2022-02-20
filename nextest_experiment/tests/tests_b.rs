9cc379e40a62495b50ce0de0b2849264fda575d4
Michael Abel <info@abel-ikt.de>
2022-02-20 18:16:49 +0100

    Clone some unit-tests

---
+++ nextest_experiment/tests/tests_b.rs
@@ -0,0 +1,16 @@
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_1(){}
+    #[test]
+    fn test_2(){}
+    #[test]
+    fn test_3(){}
+    #[test]
+    fn test_4(){}
+    #[test]
+    fn test_5(){}
+
+}
