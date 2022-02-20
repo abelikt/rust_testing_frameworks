d0dbad5d1dba1bdbb6ce57955037b68d7a17b747
Michael Abel <info@abel-ikt.de>
2022-02-20 17:34:20 +0100

    Avoid test generator macro for now

--- nextest_experiment/src/main.rs
+++ nextest_experiment/src/main.rs
@@ -1,29 +1,9 @@
 extern crate proc_macro;
 
-use proc_macro::TokenStream;
-
 fn main() {
     println!("Hello, world!");
 }
 
-/*
-Should have been a macro that generates testcases, but does not work yet
-#[macro_export]
-macro_rules! multiply {
-    let concatenated = format!("multitest__{}", $x);
-    let varname = syn::Ident::new(&concatenated, $x.span());
-    ( $( $x:expr ),* ) =>{
-        $(
-    #[test]
-        quote! { fn #varname() {
-        }
-        }
-        )*
-    };
-}
-
-multiply![1,2,3,4];
-*/
 
 #[cfg(test)]
 mod tests {
