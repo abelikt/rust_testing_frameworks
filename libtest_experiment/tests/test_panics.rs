743dd93a0f08c6ef5f7e8c6ff632d9ae030b5bf4
Michael Abel <mabel@bitmuster.org>
2025-07-22 22:03:32 +0200

    Fix tests and avoid warnings

--- libtest_experiment/tests/test_panics.rs
+++ libtest_experiment/tests/test_panics.rs
@@ -1,8 +1,6 @@
-
 // cargo test test_panic -- --nocapture
 
 // cargo test test_panic -- --nocapture --test-threads 1
-
 
 #[cfg(test)]
 mod panic_tests {
@@ -18,34 +16,29 @@ mod panic_tests {
         println!("Four");
     }
 
+    #[should_panic]
     #[test]
     fn test_panic() {
         println!("One");
         println!("Two");
         panic!();
-        println!("Tree");
-        println!("Four");
     }
 
     #[test]
     fn test_panic_magic() {
         println!("One");
-        let ta = t::TestSetupMagicA::new();
+        let _ta = t::TestSetupMagicA::new();
         println!("Two");
-        //panic!();
         println!("Tree");
         println!("Four");
     }
 
+    #[should_panic]
     #[test]
     fn test_panic_magic_with_panic() {
         println!("One");
-        let ta = t::TestSetupMagicA::new();
+        let _ta = t::TestSetupMagicA::new();
         println!("Two");
         panic!();
-        println!("Tree");
-        println!("Four");
     }
-
 }
-
