72cba1a456a38aaa2c304b589339d1a2cf7a5988
Michael Abel <mabel@bitmuster.org>
2025-09-01 23:00:50 +0200

    Deviate from the most simple use-case

--- injectorpp_experiment/src/main.rs
+++ injectorpp_experiment/src/main.rs
@@ -9,7 +9,7 @@ fn try_repair() -> Result<(), String> {
     println!("Running try_repair ...");
     if let Err(e) = fs::create_dir_all("/tmp/target_files") {
         println!("Error while running try_repair (expected).");
-        return Err(format!("Could not create directory: {}", e));
+        return Err(format!("Could not create directory: {:?}", e));
     }
 
     Ok(())
@@ -18,19 +18,40 @@ fn try_repair() -> Result<(), String> {
 fn main() {
     println!("Hello, Example!");
 
-    assert!(try_repair().is_ok());
+    {
+        assert!(try_repair().is_ok());
 
-    let mut injector = InjectorPP::new();
-    injector
-        .when_called(
-            injectorpp::func!(fn (fs::create_dir_all)(&'static str) -> std::io::Result<()>),
-        )
-        .will_execute(injectorpp::fake!(
-            func_type: fn(path: &str) -> std::io::Result<()>,
-            when: path == "/tmp/target_files",
-            returns: Ok(()),
-            times: 1
-        ));
+        let mut injector = InjectorPP::new();
+        injector
+            .when_called(
+                injectorpp::func!(fn (fs::create_dir_all)(&'static str) -> std::io::Result<()>),
+            )
+            .will_execute(injectorpp::fake!(
+                func_type: fn(path: &str) -> std::io::Result<()>,
+                when: path == "/tmp/target_files",
+                returns: Ok(()),
+                times: 1
+            ));
 
-    assert!(try_repair().is_ok());
+        assert!(try_repair().is_ok());
+    }
+
+    {
+        assert!(try_repair().is_ok());
+
+        let mut injector = InjectorPP::new();
+        injector
+            .when_called(
+                injectorpp::func!(fn (fs::create_dir_all)(&'static str) -> std::io::Result<()>),
+            )
+            .will_execute(injectorpp::fake!(
+                func_type: fn(path: &str) -> std::io::Result<()>,
+                when: path == "/tmp/target_files",
+                returns: Err(std::io::Error::new(std::io::ErrorKind::Deadlock, "Oh no, a mock")),
+                times: 2
+            ));
+
+        assert!(try_repair().is_err());
+        println!("Error type: {:?}", try_repair());
+    }
 }
