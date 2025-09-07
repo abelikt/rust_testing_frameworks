3eb5e87c3501ae8eeff85449a49c6fe22570b65c
Michael Abel <mabel@bitmuster.org>
2025-09-07 11:07:55 +0200

    Add examples as tests

---
+++ injectorpp_experiment/tests/basic.rs
@@ -0,0 +1,61 @@
+// Half started experiment for injectorpp
+//
+// Blatantly copied from the docu of injectorpp
+// https://docs.rs/injectorpp/0.4.0/injectorpp/
+
+use std::fs;
+use std::io;
+
+use injectorpp::interface::injector::*;
+
+fn try_repair() -> Result<(), String> {
+    println!("Running try_repair ...");
+    if let Err(e) = fs::create_dir_all("/tmp/target_files") {
+        println!("Error while running try_repair (expected).");
+        return Err(format!("Could not create directory: {:?}", e));
+    }
+
+    Ok(())
+}
+
+// Basic example from the docu
+#[test]
+fn basici_example() {
+    assert!(try_repair().is_ok());
+
+    let mut injector = InjectorPP::new();
+    injector
+            .when_called(
+                injectorpp::func!(fn (fs::create_dir_all)(&'static str) -> std::io::Result<()>),
+            )
+            .will_execute(injectorpp::fake!(
+                func_type: fn(path: &str) -> std::io::Result<()>,
+                when: path == "/tmp/target_files",
+                returns: Ok(()),
+                times: 1
+            ));
+
+    assert!(try_repair().is_ok());
+}
+
+// Simple example with fs::exists
+#[test]
+fn basic_std_fs_exists_a() -> io::Result<()> {
+    if let Ok(false) = fs::exists("nofile.txt") {
+    } else {
+        panic!("Assumption wrong, file is existing");
+    }
+
+    let mut injector = InjectorPP::new();
+    injector
+            .when_called(
+                injectorpp::func!(fn (fs::exists)(&'static str) -> std::io::Result<bool>),
+            )
+            .will_execute(injectorpp::fake!(
+                func_type: fn(_path: &str) -> std::io::Result<bool>,
+                returns: Ok(true),
+                times: 1
+            ));
+    assert!(fs::exists("nofile.txt").is_ok());
+    Ok(())
+}
