0adaa46bcd0d1bc042cf496db72e505f6577dd76
Michael Abel <info@abel-ikt.de>
2025-12-25 18:48:03 +0100

    Refactor example

--- injectorpp_experiment/tests/test_paralell.rs
+++ injectorpp_experiment/tests/test_paralell.rs
@@ -16,22 +16,38 @@ fn gen_injector() -> InjectorPP {
         .when_called(injectorpp::func!(fn (simple_dut_dependency)(u32) -> u32 ))
         .will_execute(injectorpp::fake!(
             func_type: fn(input:u32) -> u32,
-            returns: 88,
+            returns: 8,
             times: 1
         ));
     injector
 }
 
+#[ignore]
 #[test]
 fn test_simple_dependency() {
-    assert_eq!(dut_simple(11), 66);
+    assert_eq!(dut_simple(2), 12);
     let _injector = gen_injector();
-    assert_eq!(dut_simple(11), 264);
+    assert_eq!(dut_simple(2), 24);
+}
+
+#[ignore]
+#[test]
+fn test_simple_dependency_2() {
+    assert_eq!(dut_simple(2), 12);
+    let _injector = gen_injector();
+    assert_eq!(dut_simple(2), 24);
 }
 
 #[test]
-fn test_simple_dependency_2() {
-    assert_eq!(dut_simple(11), 66);
-    let _injector = gen_injector();
-    assert_eq!(dut_simple(11), 264);
+fn test_simple_dependency_all() {
+    {
+        assert_eq!(dut_simple(2), 12);
+        let _injector = gen_injector();
+        assert_eq!(dut_simple(2), 24);
+    }
+    {
+        assert_eq!(dut_simple(2), 12);
+        let _injector = gen_injector();
+        assert_eq!(dut_simple(2), 24);
+    }
 }
