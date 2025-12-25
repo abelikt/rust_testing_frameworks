96ca8d4256910a4da410b4103dd7ac57bd21472d
Michael Abel <info@abel-ikt.de>
2025-12-25 21:48:09 +0100

    Refactor main

--- injectorpp_experiment/src/main.rs
+++ injectorpp_experiment/src/main.rs
@@ -3,6 +3,10 @@
 // Blatantly copied from the docu of injectorpp
 // https://docs.rs/injectorpp/0.4.0/injectorpp/
 
+// Observe renerated code with:
+//
+// cargo rustc --profile=check -- -Zunpretty=expanded > expand.rs
+
 use std::fs;
 
 use injectorpp::interface::injector::*;
@@ -17,43 +21,66 @@ fn try_repair() -> Result<(), String> {
     Ok(())
 }
 
+fn example_a() {
+    assert!(try_repair().is_ok());
+
+    let mut injector = InjectorPP::new();
+    injector
+        .when_called(
+            injectorpp::func!(fn (fs::create_dir_all)(&'static str) -> std::io::Result<()>),
+        )
+        .will_execute(injectorpp::fake!(
+            func_type: fn(path: &'static str) -> std::io::Result<()>,
+            when: path == "/tmp/target_files",
+            returns: Ok(()),
+            times: 1
+        ));
+
+    assert!(try_repair().is_ok());
+}
+
+fn example_b() {
+    assert!(try_repair().is_ok());
+
+    let mut injector = InjectorPP::new();
+    injector
+        .when_called(
+            injectorpp::func!(fn (fs::create_dir_all)(&'static str) -> std::io::Result<()>),
+        )
+        .will_execute(injectorpp::fake!(
+            func_type: fn(path: &'static str) -> std::io::Result<()>,
+            when: path == "/tmp/target_files",
+            returns: Err(std::io::Error::new(std::io::ErrorKind::Deadlock, "Oh no, a mock")),
+            times: 2
+        ));
+
+    assert!(try_repair().is_err());
+    println!("Error type: {:?}", try_repair());
+}
+fn simple_dut_dependency(input: u32) -> u32 {
+    input * 2
+}
+
+fn dut_simple(input: u32) -> u32 {
+    simple_dut_dependency(input) * 3
+}
+
+fn example_c() {
+    let mut injector = InjectorPP::new();
+    injector
+        .when_called(injectorpp::func!(fn (simple_dut_dependency)(u32) -> u32 ))
+        .will_execute(injectorpp::fake!(
+            func_type: fn(input:u32) -> u32,
+            returns: 88,
+            times: 1
+        ));
+
+    assert_eq!(dut_simple(11), 264);
+}
+
 fn main() {
     println!("Hello, Example!");
-
-    {
-        assert!(try_repair().is_ok());
-
-        let mut injector = InjectorPP::new();
-        injector
-            .when_called(
-                injectorpp::func!(fn (fs::create_dir_all)(&'static str) -> std::io::Result<()>),
-            )
-            .will_execute(injectorpp::fake!(
-                func_type: fn(path: &'static str) -> std::io::Result<()>,
-                when: path == "/tmp/target_files",
-                returns: Ok(()),
-                times: 1
-            ));
-
-        assert!(try_repair().is_ok());
-    }
-
-    {
-        assert!(try_repair().is_ok());
-
-        let mut injector = InjectorPP::new();
-        injector
-            .when_called(
-                injectorpp::func!(fn (fs::create_dir_all)(&'static str) -> std::io::Result<()>),
-            )
-            .will_execute(injectorpp::fake!(
-                func_type: fn(path: &'static str) -> std::io::Result<()>,
-                when: path == "/tmp/target_files",
-                returns: Err(std::io::Error::new(std::io::ErrorKind::Deadlock, "Oh no, a mock")),
-                times: 2
-            ));
-
-        assert!(try_repair().is_err());
-        println!("Error type: {:?}", try_repair());
-    }
+    example_a();
+    example_b();
+    example_c();
 }
