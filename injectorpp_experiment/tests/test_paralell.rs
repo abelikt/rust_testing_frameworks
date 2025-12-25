80ad7bcbd3623efe9ed4b0e4f94947a20534f817
Michael Abel <info@abel-ikt.de>
2025-12-25 20:57:01 +0100

    Extend tests

--- injectorpp_experiment/tests/test_paralell.rs
+++ injectorpp_experiment/tests/test_paralell.rs
@@ -1,6 +1,7 @@
 // https://docs.rs/injectorpp/0.4.0/injectorpp/
 
 use injectorpp::interface::injector::*;
+use std::any::Any;
 
 fn simple_dut_dependency(input: u32) -> u32 {
     input * 2
@@ -39,15 +40,45 @@ fn test_simple_dependency_2() {
 }
 
 #[test]
+fn test_simple_dependency_all_injected() {
+    {
+        assert_eq!(dut_simple(2), 12);
+        let injector = gen_injector();
+        assert_eq!(dut_simple(2), 24);
+        println!("{:?}", injector.type_id()); // Make sure it still exists
+    }
+    {
+        assert_eq!(dut_simple(2), 12);
+        let injector = gen_injector();
+        assert_eq!(dut_simple(2), 24);
+        println!("{:?}", injector.type_id()); // Make sure it still exists // Make sure it still exists
+    }
+}
+
+#[test]
 fn test_simple_dependency_all() {
     {
         assert_eq!(dut_simple(2), 12);
-        let _injector = gen_injector();
+        let mut injector = InjectorPP::new();
+        injector
+            .when_called(injectorpp::func!(fn (simple_dut_dependency)(u32) -> u32 ))
+            .will_execute(injectorpp::fake!(
+                func_type: fn(input:u32) -> u32,
+                returns: 8,
+                times: 1
+            ));
         assert_eq!(dut_simple(2), 24);
     }
     {
         assert_eq!(dut_simple(2), 12);
-        let _injector = gen_injector();
+        let mut injector = InjectorPP::new();
+        injector
+            .when_called(injectorpp::func!(fn (simple_dut_dependency)(u32) -> u32 ))
+            .will_execute(injectorpp::fake!(
+                func_type: fn(input:u32) -> u32,
+                returns: 8,
+                times: 1
+            ));
         assert_eq!(dut_simple(2), 24);
     }
 }
