39b9c1bf12eaf368488d371b77f251feaeaafc70
Michael Abel <info@abel-ikt.de>
2026-06-21 23:34:23 +0200

    Extract patch guard generation

--- injectorpp_experiment/tests/extract_mock.rs
+++ injectorpp_experiment/tests/extract_mock.rs
@@ -23,7 +23,7 @@ fn command_caller_stdout() -> (String, String) {
 }
 
 #[test]
-fn command_caller_output() {
+fn test_command_caller_output() {
     let (stdout, stderr) = command_caller_stdout();
     assert!(stdout.contains("Cargo.toml"));
     assert!(stderr == "");
@@ -46,6 +46,46 @@ fn command_caller_output() {
                 ),
                 times: 1
             ));
+        // act
+        let (stdout, stderr) = command_caller_stdout();
+        // assert
+        println!("Stdout : {stdout}");
+        assert_eq!(stdout, "Hello world!");
+        assert_eq!(stderr, "Hello Errors!");
+    }
+}
+
+fn patch_command_output() -> InjectorPP {
+    let mut injector = InjectorPP::new();
+    injector
+            .when_called(
+                injectorpp::func!( fn(process::Command::output)( &mut process::Command )
+                -> io::Result<process::Output>),
+            )
+            .will_execute(injectorpp::fake!(
+                func_type: fn( _cmd: &mut process::Command )
+                    -> io::Result<process::Output>,
+                    returns: Ok(process::Output {
+                        status: process::ExitStatus::from_raw(42),
+                        stdout: Vec::from("Hello world!"),
+                        stderr: Vec::from("Hello Errors!"),
+                    }
+                ),
+                times: 1
+            ));
+    injector
+}
+
+#[test]
+fn test_command_caller_output_extracted() {
+    let (stdout, stderr) = command_caller_stdout();
+    assert!(stdout.contains("Cargo.toml"));
+    assert!(stderr == "");
+    {
+        // arrange
+        // Just assigning _ does not seem work.
+        // The patch guard  is maybe not even stored in that case
+        let _injector = patch_command_output();
         // act
         let (stdout, stderr) = command_caller_stdout();
         // assert
